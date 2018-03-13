aa5e65b9bd4e7e9c77db48b7eb74872f076eea4d
Ankirama <fabien.martinez@epitech.eu>
2018-03-13 16:48:10 +0100

    [Textfields]: Customize to add padding

---
+++ ios_project/UICustomClasses/TextField.swift
@@ -0,0 +1,25 @@
+//
+//  TextField.swift
+//  iosPocketcity
+//
+//  Created by Soomin Lee on 10/10/16.
+//  Copyright © 2016 Soomin Lee. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class TextField: UITextField {
+    @IBInspectable var insetX: CGFloat = 0
+    @IBInspectable var insetY: CGFloat = 0
+    
+    // placeholder position
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: insetX , dy: insetY)
+    }
+    
+    // text position
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: insetX , dy: insetY)
+    }
+}
