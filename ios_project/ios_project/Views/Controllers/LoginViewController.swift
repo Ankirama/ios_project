f7ed80b2e6e7ab5ed349097e8669d56d505cb1f4
Ankirama <fabien.martinez@epitech.eu>
2018-03-14 23:10:14 +0100

    [ViewControllers]: Implement model's functions

--- ios_project/ios_project/Views/Controllers/LoginViewController.swift
+++ ios_project/ios_project/Views/Controllers/LoginViewController.swift
@@ -7,8 +7,6 @@
 //
 
 import UIKit
-import Firebase
-import FirebaseAuth
 
 internal final class LoginViewController: UIViewController {
   
@@ -16,37 +14,53 @@ internal final class LoginViewController: UIViewController {
   @IBOutlet weak var signInButton: UIButton!
   @IBOutlet weak var emailTextField: UITextField!
   @IBOutlet weak var passwordTextField: UITextField!
+  @IBOutlet weak var darkView: UIView!
+  @IBOutlet weak var spinner: UIActivityIndicatorView!
 
-  @IBAction func createAccountAction(_ sender: AnyObject) {
-    
-    if emailTextField.text == "" {
-      let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
-      
-      let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-      alertController.addAction(defaultAction)
-      
-      present(alertController, animated: true, completion: nil)
-      
+  //MARK: - Alert Controller
+  func showAlertController(message: String){
+    let alertController : UIAlertController = UIAlertController(title: "Error!", message: message, preferredStyle: UIAlertControllerStyle.alert)
+    let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel) { (action) in
+      return
+    }
+    alertController.addAction(okAction)
+    self.present(alertController, animated: true, completion: nil)
+  }
+  
+  func showLoading(state: Bool)  {
+    if state {
+      self.darkView.isHidden = false
+      self.spinner.startAnimating()
+      UIView.animate(withDuration: 0.3, animations: {
+        self.darkView.alpha = 0.5
+      })
     } else {
-      Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
-        
+      UIView.animate(withDuration: 0.3, animations: {
+        self.darkView.alpha = 0
+      }, completion: { _ in
+        self.spinner.stopAnimating()
+        self.darkView.isHidden = true
+      })
+    }
+  }
+  
+  @IBAction func loginAction(_ sender: AnyObject) {
+    if passwordTextField.text == "" || emailTextField.text == "" {
+      self.showAlertController(message: "You should fill all fields")
+      return
+    }
+    self.showLoading(state: true)
+    UserDataSource.loginUser(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (error) in
+      self.showLoading(state: false)
+      DispatchQueue.main.async {
         if error == nil {
-          print("You have successfully signed up")
-          //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
-          
-          let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
-          self.present(vc!, animated: true, completion: nil)
-          
+          let vc = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "Home") as! HomeViewController
+          self.present(vc, animated: true, completion: nil)
         } else {
-          let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-          
-          let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-          alertController.addAction(defaultAction)
-          
-          self.present(alertController, animated: true, completion: nil)
+          self.showAlertController(message: error!)
         }
       }
-    }
+    })
   }
   
   override func viewDidLoad() {
--- ios_project/ios_project/Views/Controllers/SignupViewController.swift
+++ ios_project/ios_project/Views/Controllers/SignupViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
-import FirebaseAuth
+import Photos
 
 internal final class SignupViewController: UIViewController {
   
   @IBOutlet weak var signInButton: UIButton!
   @IBOutlet weak var emailTextField: UITextField!
   @IBOutlet weak var passwordTextField: UITextField!
-  @IBOutlet weak var password2TextField: UITextField!
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var profilePicView: UIImageView!
+  @IBOutlet weak var darkView: UIView!
+  @IBOutlet weak var spinner: UIActivityIndicatorView!
+  let imagePicker = UIImagePickerController()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,7 +31,6 @@ internal final class SignupViewController: UIViewController {
   }
   
   //MARK: - Alert Controller
-  
   func showAlertController(message: String){
     let alertController : UIAlertController = UIAlertController(title: "Error!", message: message, preferredStyle: UIAlertControllerStyle.alert)
     let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel) { (action) in
@@ -37,21 +40,74 @@ internal final class SignupViewController: UIViewController {
     self.present(alertController, animated: true, completion: nil)
   }
   
+  func openPhotoPickerWith(source: ImageSource) {
+    switch source {
+    case .camera:
+      let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+      if (status == .authorized || status == .notDetermined) {
+        self.imagePicker.sourceType = .camera
+        self.imagePicker.allowsEditing = true
+        self.present(self.imagePicker, animated: true, completion: nil)
+      }
+    case .library:
+      let status = PHPhotoLibrary.authorizationStatus()
+      if (status == .authorized || status == .notDetermined) {
+        self.imagePicker.sourceType = .savedPhotosAlbum
+        self.imagePicker.allowsEditing = true
+        self.present(self.imagePicker, animated: true, completion: nil)
+      }
+    }
+  }
+  
+  func showLoading(state: Bool)  {
+    if state {
+      self.darkView.isHidden = false
+      self.spinner.startAnimating()
+      UIView.animate(withDuration: 0.3, animations: {
+        self.darkView.alpha = 0.5
+      })
+    } else {
+      UIView.animate(withDuration: 0.3, animations: {
+        self.darkView.alpha = 0
+      }, completion: { _ in
+        self.spinner.stopAnimating()
+        self.darkView.isHidden = true
+      })
+    }
+  }
+  
+  @IBAction func selectPictureAction(_ sender: Any) {
+    let sheet = UIAlertController(title: nil, message: "Select the source", preferredStyle: .actionSheet)
+    let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {
+      (alert: UIAlertAction!) -> Void in
+      self.openPhotoPickerWith(source: .camera)
+    })
+    let photoAction = UIAlertAction(title: "Gallery", style: .default, handler: {
+      (alert: UIAlertAction!) -> Void in
+      self.openPhotoPickerWith(source: .library)
+    })
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    sheet.addAction(cameraAction)
+    sheet.addAction(photoAction)
+    sheet.addAction(cancelAction)
+    self.present(sheet, animated: true, completion: nil)
+  }
+  
   @IBAction func signUpAction(_ sender: AnyObject) {
-    if passwordTextField.text == "" || password2TextField.text == "" || emailTextField.text == "" {
+    if passwordTextField.text == "" || nameTextField.text == "" || emailTextField.text == "" {
       self.showAlertController(message: "You should fill all fields")
       return
     }
-    if password2TextField.text != passwordTextField.text {
-      self.showAlertController(message: "Your 2 passwords must match ")
-    }
-    Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
-      if error != nil {
-        self.showAlertController(message: (error?.localizedDescription)!)
-      } else {
-        print("Account created")
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
-        self.present(vc!, animated: true, completion: nil)
+    self.showLoading(state: true)
+    UserDataSource.createUser(withName: self.nameTextField.text!, email: self.emailTextField.text!, password: self.passwordTextField.text!, profilePic: self.profilePicView.image!) { (error) in
+      self.showLoading(state: false)
+      DispatchQueue.main.async {
+        if error == nil {
+          let vc = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "Home") as! HomeViewController
+          self.present(vc, animated: true, completion: nil)
+        } else {
+          self.showAlertController(message: error!)
+        }
       }
     }
   }
